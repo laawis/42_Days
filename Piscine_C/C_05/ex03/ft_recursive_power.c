009747cf42c33a3399588c611055d9067a77179f
laawis <115143274+laawis@users.noreply.github.com>
2023-07-17 12:53:23 +0200

    Update ft_recursive_power.c

--- Piscine_C/C_05/ex03/ft_recursive_power.c
+++ Piscine_C/C_05/ex03/ft_recursive_power.c
@@ -14,7 +14,7 @@ int	ft_recursive_power(int nb, int power)
 {
 	if (power < 0)
 		return (0);
-	if (!power)
+	if (power == 0);
 		return (1);
 	else
 		return (nb * ft_recursive_power(nb, power - 1));
